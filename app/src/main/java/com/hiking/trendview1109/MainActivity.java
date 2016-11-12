eb910abc7529e6870e5cc8af9038e804a2ebc73b
lizhongli <646832570@qq.com>
2016-11-12 15:36:39 +0800

    20161112 hiking

--- app/src/main/java/com/hiking/trendview1109/MainActivity.java
+++ app/src/main/java/com/hiking/trendview1109/MainActivity.java
@@ -3,6 +3,9 @@ package com.hiking.trendview1109;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * hj 20161112
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
